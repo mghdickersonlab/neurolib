7a6f2a47e665eca6e26de251461398122f990c2d
Ryan Eckbo <ryan.eckbo@gmail.com>
2019-02-28 12:28:13 -0500

    STY: remove outdated .sh script

--- bin/ecat_to_nifti.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-BIN_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-PACKAGE_PATH="$BIN_PATH/.."
-
-source "$PACKAGE_PATH"/.venv/bin/activate
-
-PIPENV_VENV_IN_PROJECT=1 PYTHONPATH=$PACKAGE_PATH "$BIN_PATH"/ecat_to_nifti.py $*
