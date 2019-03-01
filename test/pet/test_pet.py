1dff1bc136e541596b55433c81d41ef7c3da58e9
Ryan Eckbo <ryan.eckbo@gmail.com>
2019-03-01 08:03:15 -0500

    TEST: update pet test to use ecat_to_nifti.py script

--- test/pet/test_pet.py
+++ test/pet/test_pet.py
@@ -2,15 +2,16 @@ import filecmp
 
 from plumbum import local
 
-from neurolib import pet
-
 THIS_DIR = local.path(__file__).parent
 TEST_DATA = THIS_DIR / 'data'
 
-def test_convert_ecat_to_nifti():
+
+def test_convert_ecat_to_nifti_script():
+    script = local['ecat_to_nifti.py']
     with local.tempdir() as tmpdir:
         test_input = TEST_DATA / 'fdg.v'
         test_output = tmpdir / 'ecat.nii.gz'
         expected_output = TEST_DATA / 'fdg.nii.gz'
-        pet.convert_ecat_to_nifti(test_input, test_output)
+        script('-i', test_input, '-o', test_output)
+        # pet.convert_ecat_to_nifti(test_input, test_output)
         filecmp.cmp(test_output, expected_output)
